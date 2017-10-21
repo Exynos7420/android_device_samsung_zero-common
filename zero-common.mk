af32fe7b58b7a0f570990f6095b35b0cecfae67b
Usaamah Patel <ussyp2015@gmail.com>
2017-10-21 18:57:54 +0100

    zero: Add init.power.rc
    Signed-off-by: Usaamah Patel <ussyp2015@gmail.com>

--- zero-common.mk
+++ zero-common.mk
@@ -207,6 +207,7 @@ PRODUCT_PACKAGES += \
     fstab.samsungexynos7420 \
     init.baseband.rc \
     init.gps.rc \
+    init.power.rc \
     init.samsungexynos7420.rc \
     init.samsungexynos7420.usb.rc \
     init.wifi.rc \
