cd3ef788af66e8c136f33606a7b8d37360bd5318
Usaamah Patel <ussyp2015@gmail.com>
2017-12-24 20:39:13 +0000

    zero: Fix Capacitive Buttons for S6 Edge
    Change-Id: I6255e72ecd9dc10b1a81d7f861fdc276c1892cc8
    Signed-off-by: Usaamah Patel <ussyp2015@gmail.com>

--- zero-common.mk
+++ zero-common.mk
@@ -126,6 +126,7 @@ PRODUCT_PACKAGES += \
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl \
+    $(LOCAL_PATH)/keylayout/sec_touchscreen.kl:system/usr/keylayout/sec_touchscreen.kl \
     $(LOCAL_PATH)/idc/Synaptics_HID_TouchPad.idc:system/usr/idc/Synaptics_HID_TouchPad.idc \
     $(LOCAL_PATH)/idc/ft5x06_ts.idc:system/usr/idc/ft5x06_ts.idc \
     $(LOCAL_PATH)/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc
