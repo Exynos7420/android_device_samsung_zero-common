c095ef404434c98a08ac6b77c61d565ad92497a5
Lukas0610 <mail@lukasberger.at>
2018-02-01 19:25:13 +0100

    zero: remove binderized audio-server
    Keeps libsec-ril from working properly due to its IPC permission-check failing (android.hardware.audio@2.0-service isn't allowed)
    
    Change-Id: I4215953f1382a8f01f0d7b7e47c289b296569028

--- zero-common.mk
+++ zero-common.mk
@@ -90,7 +90,6 @@ PRODUCT_PACKAGES += \
     tinymix \
     libtinycompress \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl
 
 # Bluetooth
