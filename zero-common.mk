c82bdeb66d36d69aee8d696ffb1256a4cf409a87
Lukas0610 <mail@lukasberger.at>
2018-03-01 21:11:00 +0100

    zero: Switch to TeamNexus Audio Hal
    Change-Id: I6bc963a1ac305c5b4511603ff629a1936fd250da

--- zero-common.mk
+++ zero-common.mk
@@ -80,7 +80,9 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/mixer_paths_0.xml:system/etc/mixer_paths_0.xml \
+    $(LOCAL_PATH)/configs/mixer_paths_0-audience.xml:system/etc/mixer_paths_0-audience.xml
 
 PRODUCT_PACKAGES += \
     audio.primary.universal7420 \
