5f63356a4d45ec9aa49c82a46b926abc355cc2bd
enesuzun2002 <4lukankalar@gmail.com>
2018-10-01 18:58:08 +0300

    zero: Copy audio_effects.conf to /system/vendor/etc
    Change-Id: I3c1d6240507162ca925db6ad38cb848e00309fa4

--- zero-common.mk
+++ zero-common.mk
@@ -79,7 +79,8 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/vendor/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/mixer_paths_0.xml:system/vendor/etc/mixer_paths_0.xml \
     $(LOCAL_PATH)/configs/mixer_paths_0-audience.xml:system/vendor/etc/mixer_paths_0-audience.xml
 
