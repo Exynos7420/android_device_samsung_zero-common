22e3ae8affb5b9fb7607c619318b0bdc4adfba27
enesuzun2002 <4lukankalar@gmail.com>
2018-10-07 21:07:54 +0300

    WTF i am doing
    Change-Id: I0306dfef947f6e01ee443379aa0bd550bad8a6e6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,6 +18,9 @@ LOCAL_PATH := device/samsung/zero-common
 
 BOARD_VENDOR := samsung
 
+# Include path
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
