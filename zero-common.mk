3880e94013b15919cb7dbf4d1eb4c2e7181b46c8
Daniel Jarai <jaraidaniel@gmail.com>
2018-11-30 17:00:59 +0100

    zero: Make the external camera provider ignore internal cameras
    * The external camera provider occupies our camera v4l2 nodes,
      potentially crashing the kernel driver and blocking the camera HAL.
      Unfortunately, there's no easy way to just disable it, so let's just
      tell it to ignore the internal video devices.
    
    Change-Id: I00c67de1b94a1f7eca1bf458d1d0e4de3507aa81
    Signed-off-by: Daniel Jarai <jaraidaniel@gmail.com>

--- zero-common.mk
+++ zero-common.mk
@@ -115,6 +115,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
     android.hardware.camera.provider@2.4-service
 
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/external_camera_config.xml:system/vendor/etc/external_camera_config.xml
+
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-impl \
