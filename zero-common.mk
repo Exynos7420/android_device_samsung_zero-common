9d5e14e3726d28bd3d2350d6827565d04b9e5788
enesuzun2002 <enesuzun200227@gmail.com>
2019-03-23 16:39:58 +0300

    zero: update and enable camera wrapper

--- zero-common.mk
+++ zero-common.mk
@@ -103,6 +103,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.exynos5 \
     libexynoscamera_shim \
     android.hardware.camera.provider@2.4-impl.zero \
     android.hardware.camera.provider@2.4-service
