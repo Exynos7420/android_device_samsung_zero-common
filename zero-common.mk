5a41d014a28e5f429cb9b0453b805852157e865d
Jan Altensen <info@stricted.net>
2019-07-10 03:24:25 +0200

    zero: build new hidl based lights hal
    Change-Id: I9ef58054bf2eb44238db00e7814137f5c9b0a672

--- zero-common.mk
+++ zero-common.mk
@@ -209,9 +209,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    lights.universal7420 \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.light@2.0-service.samsung
 
 
 # Memory
