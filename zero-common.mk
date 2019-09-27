48dacda0187cc259fa9d7352696757ab711544c3
enesuzun2002 <enesuzun200227@gmail.com>
2019-09-27 04:27:53 +0000

    zero: camera: convert to Android.bp
    Change-Id: I3f3649f588b5b5cdcfcb5daa765276ee2b81ab18

--- zero-common.mk
+++ zero-common.mk
@@ -111,7 +111,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     camera.exynos5 \
     libexynoscamera_shim \
-    android.hardware.camera.provider@2.4-impl.zero \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
 PRODUCT_COPY_FILES += \
