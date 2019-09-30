d28bab56ea00820b9975e4e1dc9d72209406dd66
Bruno Martins <bgcngm@gmail.com>
2019-09-30 13:32:04 +0100

    zero: Drop mediaextractor seccomp policy
    These syscalls are now allowed by default.
    
    Change-Id: I405f609d030331e19031612cbd912a54a3c72339

--- zero-common.mk
+++ zero-common.mk
@@ -325,8 +325,7 @@ PRODUCT_PACKAGES += \
 
 # seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
 
 # Sensorhub
 PRODUCT_PACKAGES += \
