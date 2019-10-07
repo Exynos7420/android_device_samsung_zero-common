d4dc5f37306630cc87f7313f81111b6afde4a869
enesuzun2002 <enesuzun200227@gmail.com>
2019-10-07 07:33:54 +0000

    zero: use legacy camera provider

--- zero-common.mk
+++ zero-common.mk
@@ -112,7 +112,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     camera.exynos5 \
     libexynoscamera_shim \
-    android.hardware.camera.provider@2.4-impl.zero \
+    android.hardware.camera.provider@2.4-impl-legacy \
     android.hardware.camera.provider@2.4-service
 
 # Configstore
