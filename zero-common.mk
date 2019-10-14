6ca583fa5a8dd3bc7a852f6f7ee16600029f38e7
enesuzun2002 <enesuzun200227@gmail.com>
2019-10-14 09:43:15 +0000

    Revert "zero: use legacy camera provider"
    This reverts commit d4dc5f37306630cc87f7313f81111b6afde4a869.

--- zero-common.mk
+++ zero-common.mk
@@ -112,10 +112,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     camera.exynos5 \
     libexynoscamera_shim \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl.zero \
     android.hardware.camera.provider@2.4-service \
     Snap
-
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/external_camera_config.xml:system/vendor/etc/external_camera_config.xml
