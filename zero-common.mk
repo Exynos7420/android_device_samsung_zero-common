c148df7a603fbcec76990b8be3dc1d20b9719400
enesuzun2002 <enesuzun200227@gmail.com>
2019-10-14 20:05:36 +0000

    zero: Don't build Snap Camera

--- zero-common.mk
+++ zero-common.mk
@@ -113,8 +113,7 @@ PRODUCT_PACKAGES += \
     camera.exynos5 \
     libexynoscamera_shim \
     android.hardware.camera.provider@2.4-impl.zero \
-    android.hardware.camera.provider@2.4-service \
-    Snap
+    android.hardware.camera.provider@2.4-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/external_camera_config.xml:system/vendor/etc/external_camera_config.xml
