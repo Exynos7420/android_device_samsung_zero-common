499428808e8a00be7b477f923397a3d89429f4b9
Danny Wood <danwood76@gmail.com>
2019-12-23 12:00:00 +0000

    zero: fingerprint: switch to using the new HIDL based hal
    Change-Id: I70c3f33e1979637cee343601a1a0fa0074cacb86

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,6 +40,7 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
 TARGET_NR_CPUS := 8
 
 # Audio
+TARGET_AUDIOHAL_VARIANT := samsung
 USE_XML_AUDIO_POLICY_CONF := 1
 
 # Binder
@@ -104,6 +105,10 @@ TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY := //$(LOCAL_PATH):libcamera_parameters
 TARGET_LD_SHIM_LIBS += \
 	/system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so \
 	/system/lib64/libexynoscamera.so|/vendor/lib64/libexynoscamera_shim.so
+
+# Fingerprint
+TARGET_SEC_FP_CALL_NOTIFY_ON_CANCEL := true
+TARGET_SEC_FP_USES_PERCENTAGE_SAMPLES := true
 
 # Fingerprint-shims
 TARGET_LD_SHIM_LIBS += \
@@ -220,10 +225,6 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3124019200
 TARGET_BOARD_PLATFORM := exynos5
 TARGET_SOC := exynos7420
 TARGET_SLSI_VARIANT := cm
-
-# Samsung HAL's
-TARGET_SEC_FP_HAL_VARIANT := bauth
-TARGET_AUDIOHAL_VARIANT := samsung
 
 # Radio
 BOARD_PROVIDES_LIBRIL := true
