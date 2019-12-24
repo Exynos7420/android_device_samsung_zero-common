efa184e1e36afb6dc2fcff6cea47cf26d18dcbda
enesuzun2002 <enesuzun200227@gmail.com>
2019-12-24 18:31:13 +0000

    zero: Goodbye F-Droid

--- zero-common.mk
+++ zero-common.mk
@@ -136,12 +136,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ZeroDoze
 
-##################
-# F-Droid
-PRODUCT_PACKAGES += \
-    FDroid \
-    F-DroidPrivilegedExtension
-
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     make_ext4fs \
