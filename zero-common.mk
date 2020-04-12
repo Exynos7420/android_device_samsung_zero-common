3164e4216a3dca04f0ce2af20ca715f3f853eabc
enesuzun2002 <enesuzun200227@gmail.com>
2020-04-12 15:26:32 +0000

    zero: fck off camera wrapper
    Change-Id: I769fc587423369742cd32949d605f22e89bc5b73

--- zero-common.mk
+++ zero-common.mk
@@ -123,7 +123,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-#   camera.exynos5 \
     libexynoscamera_shim \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
