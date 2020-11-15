da862254f06bcbfe8541a2b2f9f5f148582928a1
enesuzun2002 <enesmuratuzun@outlook.com.tr>
2020-11-15 11:11:28 +0300

    zerofltexx: build IR packages
    Change-Id: Ic3a8797dafbea4dce26f335c9c66ebdc4ddfaad9

--- device.mk
+++ device.mk
@@ -16,6 +16,10 @@
 
 LOCAL_PATH := device/samsung/zerofltexx
 
+# Permissions
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+
 # Audio
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml
@@ -28,6 +32,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from universal7420-common
 $(call inherit-product, device/samsung/universal7420-common/device-common.mk)
+
+# IR
+PRODUCT_PACKAGES += \
+	android.hardware.ir@1.0-impl \
+	android.hardware.ir@1.0-service
 
 # Call the proprietary
 $(call inherit-product, vendor/samsung/zerofltexx/zerofltexx-vendor.mk)
