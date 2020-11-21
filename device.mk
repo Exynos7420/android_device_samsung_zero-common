75d51369aae7fb5258be316b52989f1a86fbdb96
enesuzun2002 <enesmuratuzun@outlook.com.tr>
2020-11-21 08:14:35 +0000

    zerofltexx: add AAPT configuration
    Change-Id: I99035d571b70960c0905cf2d6a836eaf71a8e7d0

--- device.mk
+++ device.mk
@@ -20,10 +20,20 @@ LOCAL_PATH := device/samsung/zerofltexx
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# AAPT
+# This device is 640dpi.  However the platform doesn't
+# currently contain all of the bitmaps at 640dpi density so
+# we do this little trick to fall back to the xxhdpi version
+# if the 640dpi doesn't exist.
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := 640dpi
+# A list of dpis to select prebuilt apk, in precedence order.
+PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
+
 # Audio
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml
-    
+
 # HIDL Manifest
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/manifest.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml
