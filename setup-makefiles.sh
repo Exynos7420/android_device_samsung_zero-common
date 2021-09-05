225c39dbf28923c358587878df01e2efd63cf2ac
filiprrs <filiprrs@gmail.com>
2021-09-05 13:06:39 +0200

    zerofltexx: Rework EGL symlinking
    Change-Id: I6c0b3136acc895429d047d4635f8d2e953469650

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -34,5 +34,38 @@ write_headers
 
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
-# Finish
+###################################################################################################
+# CUSTOM PART START                                                                               #
+###################################################################################################
+
+OUTDIR=vendor/$VENDOR/$DEVICE
+
+(cat << EOF) >> $ANDROID_ROOT/$OUTDIR/Android.mk
+include \$(CLEAR_VARS)
+
+EGL_LIBS := libGLES_mali.so libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1 vulkan.exynos5.so
+
+EGL_32_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib/,\$(EGL_LIBS))
+\$(EGL_32_SYMLINKS): \$(LOCAL_INSTALLED_MODULE)
+	@echo "Symlink: EGL 32-bit lib: \$@"
+	@mkdir -p \$(dir \$@)
+	@rm -rf \$@
+	\$(hide) ln -sf /vendor/lib/egl/libGLES_mali.so \$@
+
+EGL_64_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib64/,\$(EGL_LIBS))
+\$(EGL_64_SYMLINKS): \$(LOCAL_INSTALLED_MODULE)
+	@echo "Symlink: EGL 64-bit lib : \$@"
+	@mkdir -p \$(dir \$@)
+	@rm -rf \$@
+	\$(hide) ln -sf /vendor/lib64/egl/libGLES_mali.so \$@
+
+ALL_DEFAULT_INSTALLED_MODULES += \$(EGL_32_SYMLINKS) \$(EGL_64_SYMLINKS)
+
+EOF
+
+###################################################################################################
+# CUSTOM PART END                                                                                 #
+###################################################################################################
+
+# Done
 write_footers
